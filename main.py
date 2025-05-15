bec88223a71997a67043affe43938dab9b535011
Quality Gatekeeper <qgintest@gmail.com>
2025-05-14 22:56:40 -0400

    day 24: nested loops practice

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100) # RGB, Grey
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    # Add the Lines
+    for y in range(21, 290, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     pdf.ln(265)  # break lines
@@ -23,6 +26,11 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # Add the Lines
+        for y in range(11, 290, 10):
+            pdf.line(10, y, 200, y)
+
         pdf.ln(277)
 
         pdf.set_font(family="Times", style="I", size=8)
